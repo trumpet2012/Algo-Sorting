1fa486e1988c970925bb844e1543044e82093379
Trent Holliday <tholliday@morristechnology.com>
2015-08-27 09:32:58 -0400

    Ripped the individual graphs for each algorithm out into its own application. Now there is a lot of duplicated logic. Ideally this logic would be consolidated. Also would like for the graphs to be more dynamic. So that if given a dictionary of values it could plot them.

--- src/QuickSortTester.java
+++ src/QuickSortTester.java
@@ -5,48 +5,35 @@
  * Test arrays starting at 1,0000 numbers and up to 200,000 numbers.
  *
  */
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 import javafx.stage.Stage;
 
 import java.util.Arrays;
 import java.util.Random;
 
-public class SortTester extends Application{
+public class QuickSortTester extends Application{
 
-    private static SortedResult bubble1k;
-    private static SortedResult bubble10k;
-    private static SortedResult bubble100k;
-    private static SortedResult bubble200k;
-    private static SortedResult insertion1k;
-    private static SortedResult insertion10k;
-    private static SortedResult insertion100k;
-    private static SortedResult insertion200k;
-    private static SortedResult selection1k;
-    private static SortedResult selection10k;
-    private static SortedResult selection100k;
-    private static SortedResult selection200k;
     private static SortedResult quickSort1k;
     private static SortedResult quickSort10k;
     private static SortedResult quickSort100k;
     private static SortedResult quickSort200k;
 
-    final static String insertion = "Insertion";
-    final static String selection = "Selection";
     final static String quicksort = "Quick Sort";
-    final static String bubble = "Bubble";
 
     final static Random randomGenerator = new Random();
 
     private Button showGraph, showTable;
 
     public static void main(String[] args){
-        new SortTester().showGui(1);
+        new QuickSortTester().showGui(1);
         launch();
     }
 
@@ -81,23 +68,8 @@ public class SortTester extends Application{
         int[] array100k = createRandomArray(100000);
         int[] array200k = createRandomArray(200000);
 
-        Bubble bubbleSorter = new Bubble();
-        Insertion insertionSorter = new Insertion();
-        Selection selectionSorter = new Selection();
         QuickSort quickSorter = new QuickSort();
 
-        bubble1k = runAlgo(array1k, 1, bubbleSorter);
-        bubble10k = runAlgo(array10k, 1, bubbleSorter);
-        bubble100k = runAlgo(array100k, 1, bubbleSorter);
-        bubble200k = runAlgo(array200k, 1, bubbleSorter);
-        insertion1k = runAlgo(array1k, 1, insertionSorter);
-        insertion10k = runAlgo(array10k, 1, insertionSorter);
-        insertion100k = runAlgo(array100k, 1, insertionSorter);
-        insertion200k = runAlgo(array200k, 1, insertionSorter);
-        selection1k = runAlgo(array1k, 1, selectionSorter);
-        selection10k = runAlgo(array10k, 1, selectionSorter);
-        selection100k = runAlgo(array100k, 1, selectionSorter);
-        selection200k = runAlgo(array200k, 1, selectionSorter);
         quickSort1k = runAlgo(array1k, 1, quickSorter);
         quickSort10k = runAlgo(array10k, 1, quickSorter);
         quickSort100k = runAlgo(array100k, 1, quickSorter);
@@ -120,37 +92,29 @@ public class SortTester extends Application{
         final NumberAxis yAxis = new NumberAxis();
 
         final BarChart<String, Number> barChart = new BarChart<String, Number>(xAxis, yAxis);
-        barChart.setTitle("Execution times for algorithms");
+        barChart.setTitle("Execution times for Quick Sort");
 
         xAxis.setLabel("Sorting Algorithm");
         yAxis.setLabel("Speed in Microseconds");
 
         XYChart.Series<String, Number> series1k = new XYChart.Series<String, Number>();
         series1k.setName("1,000 Items");
-        series1k.getData().add(new XYChart.Data(bubble, bubble1k.getExecutionTime()));
-        series1k.getData().add(new XYChart.Data(insertion, insertion1k.getExecutionTime()));
-        series1k.getData().add(new XYChart.Data(selection, selection1k.getExecutionTime()));
+
         series1k.getData().add(new XYChart.Data(quicksort, quickSort1k.getExecutionTime()));
 
         XYChart.Series series10k = new XYChart.Series();
         series10k.setName("10,000 Items");
-        series10k.getData().add(new XYChart.Data(bubble, bubble10k.getExecutionTime()));
-        series10k.getData().add(new XYChart.Data(insertion, insertion10k.getExecutionTime()));
-        series10k.getData().add(new XYChart.Data(selection, selection10k.getExecutionTime()));
+
         series10k.getData().add(new XYChart.Data(quicksort, quickSort10k.getExecutionTime()));
 
         XYChart.Series series100k = new XYChart.Series();
         series100k.setName("100,000 Items");
-        series100k.getData().add(new XYChart.Data(bubble, bubble100k.getExecutionTime()));
-        series100k.getData().add(new XYChart.Data(insertion, insertion100k.getExecutionTime()));
-        series100k.getData().add(new XYChart.Data(selection, selection100k.getExecutionTime()));
+
         series100k.getData().add(new XYChart.Data(quicksort, quickSort100k.getExecutionTime()));
 
         XYChart.Series series200k = new XYChart.Series();
         series200k.setName("200,000 Items");
-        series200k.getData().add(new XYChart.Data(bubble, bubble200k.getExecutionTime()));
-        series200k.getData().add(new XYChart.Data(insertion, insertion200k.getExecutionTime()));
-        series200k.getData().add(new XYChart.Data(selection, selection200k.getExecutionTime()));
+
         series200k.getData().add(new XYChart.Data(quicksort, quickSort200k.getExecutionTime()));
 
         Scene graphScene = new Scene(barChart, 800, 600);
